13661e74eebdb76026f88d41bb6356fb3b8cca9f
Moin <wdmoin@gmail.com>
2020-05-23 01:10:31 +0600

    Favicon added

--- assets/js/en.js
+++ assets/js/en.js
@@ -2,6 +2,18 @@ var theater = theaterJS()
 
 var name = prompt('What is your cute nickname?');
 
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+if (hrs < 12)
+  greet = 'Good Morning';
+else if (hrs >= 12 && hrs <= 17)
+  greet = 'Good Afternoon';
+else if (hrs >= 17 && hrs <= 24)
+  greet = 'Good Evening';
+
 theater
   .addActor('name', { speed: 0.8, accuracy: true })
   .addActor('about', { speed: 1, accuracy: true })
