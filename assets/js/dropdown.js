c3a523532603e2ab1c2579b06599960bbbbeaa96
OurMoin <ourmoin@gmail.com>
2022-02-28 14:58:00 +0600

    initial project

---
+++ assets/js/dropdown.js
@@ -0,0 +1,40 @@
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("word");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("word-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("village");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("village-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("area");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("area-down");
+  });
+}
+
